9df71199e2cfe00a19ac8f560504247de8b19c8f
depedence <avergunol@yandex.ru>
2025-06-24 18:18:55 +0300

    feat: update url test

--- tests/ui-tests/url.spec.ts
+++ tests/ui-tests/url.spec.ts
@@ -3,5 +3,8 @@ import {test, expect} from '@playwright/test'
 test.describe('Проверка на загрузку страницы', () => {
     test('Проверка загрузки', async ({page}) => {
         await page.goto('/')
+        
+        await expect(page).toHaveTitle('Conduit')
+        await expect(page.getByText('conduit').nth(1)).toBeVisible()
     })
 })
