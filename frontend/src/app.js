18ae0fff8406b4838d7b6eb414fdd8657f822ca2
depedence <avergunol@yandex.ru>
2025-06-25 23:41:26 +0300

    temporary commit

---
+++ frontend/src/app.js
@@ -0,0 +1,11 @@
+import BoardPage from './pages/BoardPage';
+
+function App() {
+  return (
+    <div className="app">
+      <BoardPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="logo">BugTracker</div>
+      <button className="create-btn">+ Create</button>
+    </header>
+  );
+}
