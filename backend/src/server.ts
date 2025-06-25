ffa9d3e9b8ab4a74d40bc546b864d7e27b8fcfc3
Alexander Averin <a.averin@rosintel.com>
2025-06-25 11:55:24 +0300

    some changes

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = 3000
+
+// Middleware
+app.use(cors()) // Разрешает доступ с фронта
+app.use(express.json()) // Позволяет читать JSON в теле запроса
+
+// Примерный маршрут
+app.get('/', (req, res) => {
+    res.send('Bugpoint Tracker API is running!')
+})
+
+// Старт сервера
+app.listen(PORT, () => {
+    console.log(`Server is runnint at http://localhost:3000:${PORT}`)
+})
