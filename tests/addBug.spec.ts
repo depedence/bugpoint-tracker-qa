ec7f37ddebef800dd418a4d3011a78376931089c
Alexander Averin <a.averin@rosintel.com>
2025-07-29 13:11:29 +0300

    add bug test

--- tests/addBug.spec.ts
+++ tests/addBug.spec.ts
@@ -1,89 +1,55 @@
 import { test, expect } from '@playwright/test';
+import { beforeEach } from 'node:test';
 
-async function clickAdd(page) {
+async function tapAddBtn(page) {
   await page.getByRole('button', { name: '+' }).click();
-  await expect(page.getByRole('heading', { name: 'Добавить баг' })).toBeVisible();
-
-  await page.getByRole('textbox', { name: 'Название бага' }).fill('Тестовый баг');
-  await page.getByRole('textbox', { name: 'Описание бага' }).fill('Описание тестового бага');
-
-  await expect(page.getByRole('button', { name: 'Сохранить' })).toBeVisible();
+  await expect(page.locator('.modal-content').nth(1)).toBeVisible();
 }
 
-async function clickAdd2(page) {
-  await page.getByRole('button', { name: '+' }).click();
-  await expect(page.getByRole('heading', { name: 'Добавить баг' })).toBeVisible();
-
-  await expect(page.getByRole('button', { name: 'Сохранить' })).toBeVisible();
-  await page.getByRole('textbox', { name: 'Название бага' }).fill('Тестовый баг');
-  await page.getByRole('textbox', { name: 'Описание бага' }).fill('Описание тестового бага');
-  await page.locator('#status').selectOption('closed');
-}
-
-test.describe('Проверка на добавление бага в колонку "Открытые баги"', () => {
-  test.beforeEach('Переход на необходимый URL', async ({ page }) => {
-    console.log(`[${new Date().toISOString()}] Test started`);
+test.describe('Проверки на добавление бага @addBug', () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('/');
-    await clickAdd(page);
   });
 
-  test('Создание открытого тикета с багом низкого приоритета', async ({ page }) => {
+  test('Добавление ОТКРЫТОГО бага НИЗКОГО приоритета', async ({ page }) => {
+    await tapAddBtn(page);
+
+    await page.locator('input[type="text"]').nth(1).fill('Открытый низкий');
+    await page.getByRole('textbox', { name: 'Описание бага' }).fill('Открытый низкий описание');
+
     await page.getByRole('button', { name: 'Сохранить' }).click();
-    await expect(
-      page.getByText('x Тестовый баг Описание тестового бага Приоритет: low')
-    ).toBeVisible();
+
+    await expect(page.getByRole('heading', { name: 'Открытый низкий' })).toBeVisible();
+    console.log('! Открытый баг низкого приоритета успешно создан !');
   });
 
-  test('Создание открытого тикета с багом среднего приоритета', async ({ page }) => {
+  test('Добавление ОТКРЫТОГО бага СРЕДНЕГО приоритета', async ({ page }) => {
+    await tapAddBtn(page);
+
+    await page.locator('input[type="text"]').nth(1).fill('Открытый средний');
+    await page.getByRole('textbox', { name: 'Описание бага' }).fill('Открытый средний описание');
+
+    await page.locator('#priority').click();
     await page.locator('#priority').selectOption('medium');
+
     await page.getByRole('button', { name: 'Сохранить' }).click();
-    await expect(
-      page.getByText('x Тестовый баг Описание тестового бага Приоритет: medium')
-    ).toBeVisible();
+
+    await expect(page.getByRole('heading', { name: 'Открытый средний' })).toBeVisible();
+    console.log('! Открытый баг среднего приоритета успешно создан !');
   });
 
-  test('Создание открытого тикета с багом высокого приоритета', async ({ page }) => {
+  test('Добавление ОТКРЫТОГО бага ВЫСОКОГО приоритета', async ({ page }) => {
+    await tapAddBtn(page);
+
+    await page.locator('input[type="text"]').nth(1).fill('Открытый высокий');
+    await page.getByRole('textbox', { name: 'Описание бага' }).fill('Открытый высокий описание');
+
+    await page.locator('#priority').click();
     await page.locator('#priority').selectOption('high');
-    await page.getByRole('button', { name: 'Сохранить' }).click();
-    await expect(
-      page.getByText('x Тестовый баг Описание тестового бага Приоритет: high')
-    ).toBeVisible();
-  });
-});
 
-test.describe('Проверка на добавление бага в колонку "Закрытые баги"', () => {
-  test.beforeEach('Переход на необходимый URL', async ({ page }) => {
-    console.log(`[${new Date().toISOString()}] Test started`);
-    await page.goto('/');
-    await clickAdd2(page);
-  });
-
-  test('Создание закрытого тикета с багом низкого приоритета', async ({ page }) => {
     await page.getByRole('button', { name: 'Сохранить' }).click();
-    await expect(
-      page
-        .locator('#closed-column')
-        .getByText('x Тестовый баг Описание тестового бага Приоритет: low')
-    ).toBeVisible();
-  });
 
-  test('Создание закрытого тикета с багом среднего приоритета', async ({ page }) => {
-    await page.locator('#priority').selectOption('medium');
-    await page.getByRole('button', { name: 'Сохранить' }).click();
-    await expect(
-      page
-        .locator('#closed-column')
-        .getByText('x Тестовый баг Описание тестового бага Приоритет: medium')
-    ).toBeVisible();
-  });
-
-  test('Создание закрытого тикета с багом высокого приоритета', async ({ page }) => {
-    await page.locator('#priority').selectOption('high');
-    await page.getByRole('button', { name: 'Сохранить' }).click();
-    await expect(
-      page
-        .locator('#closed-column')
-        .getByText('x Тестовый баг Описание тестового бага Приоритет: high')
-    ).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Открытый высокий' })).toBeVisible();
+    console.log('! Открытый баг высокого приоритета успешно создан !');
   });
 });
